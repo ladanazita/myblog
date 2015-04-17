2be2deb1f24be4b6c7130c060dd61ccd4dc3de64
Ladan Nasserian <ladannasserian@gmail.com>
2015-04-17 11:36:16 -0700

    created comment model, associated with post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,7 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
+
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
@@ -31,6 +32,13 @@ class PostsController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to post_path
+  end
+
 private
   def post_params
     params.require(:post).permit(:title, :post)
