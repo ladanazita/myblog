0fc427bb300754f620bb8ed95f86b123dc8aef33
Ladan Nasserian <ladannasserian@gmail.com>
2015-04-17 10:48:23 -0700

    modified posts controller with create method, created shows for new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,23 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
+  end
+private
+  def post_params
+    params.require(:post).permit(:title, :post, :body)
   end
 end
