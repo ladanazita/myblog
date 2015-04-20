786028caa2d84f2b13f473b0ab7b85336c3bae4c
Ladan Nasserian <ladannasserian@gmail.com>
2015-04-20 16:31:22 -0700

    added authentication in application controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ end
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
 end
 
