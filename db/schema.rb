843419e2f72ec16d4734671476cf1c210cc843ce
Ladan Nasserian <ladannasserian@gmail.com>
2015-04-20 16:27:02 -0700

    installed bcrypt, created User controller, mofdified routes, created view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417183513) do
+ActiveRecord::Schema.define(version: 20150420231602) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20150417183513) do
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
